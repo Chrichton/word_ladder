9d796114c8f415408f86529887c7670526c6fd13
Heiko Goes <heiko.goes@gaebler-goes.de>
2024-06-12 12:24:02 +0200

    Validator checks that guessed_word in Words. Generate a three step ladder

--- lib/word_ladder/dictionary.ex
+++ lib/word_ladder/dictionary.ex
@@ -4,35 +4,27 @@ defmodule WordLadder.Dictionary do
   @animals ~w("bass bear beef bird boar buff bull cape carp cavy chip coot crab crow dane deer dodo dove duck fawn frog gaur gerb goat gosh gull hare hawk hind ibis jenn kang kite koal kudu lamp lark lion lynx mare mink mola mole moth mule myna newt ogre oryx ostr otus paca pika pike pony puff puma quok rabb rhea rook sand seal shad sika skua swan tahr teal tern toad vole wasp wels weta wige wolf xema zebu")
 
   def generate_word_ladders() do
-    for animal1 <- @animals, animal2 <- @animals do
-      generate_word_ladder(animal1, animal2)
+    for animal1 <- @animals,
+        animal2 <- @animals,
+        animal3 <- @animals do
+      generate_word_ladder([animal1, animal2, animal3])
     end
     |> Enum.reject(&(&1 == []))
-
-    # |> Enum.map(&generate_word_ladder_recursive/1)
   end
 
-  defp generate_word_ladder_recursive([last_animal | [pre_last_animal | _]] = animals) do
-    dbg()
-
-    if Validator.valid_move?(pre_last_animal, last_animal) do
-      [next_animal, _] = generate_word_ladder(pre_last_animal, last_animal)
-
-      [next_animal | animals]
-    else
-      animals
-    end
-  end
-
-  defp generate_word_ladder(animal1, animal2) do
-    if Validator.valid_move?(animal1, animal2) do
-      [animal2, animal1]
+  defp generate_word_ladder([animal1, animal2, animal3] = animals) do
+    if Enum.uniq(animals) == animals and
+         Validator.valid_move?(animal1, animal2) and
+         Validator.valid_move?(animal2, animal3) do
+      [animal3, animal2, animal1]
     else
       []
     end
   end
 
-  def words() do
+  def words(), do: @animals |> MapSet.new()
+
+  def words_from_file() do
     "deutsch.txt"
     |> File.stream!()
     |> Stream.map(&String.trim_trailing/1)
--- lib/word_ladder/validator.ex
+++ lib/word_ladder/validator.ex
@@ -4,6 +4,7 @@ defmodule WordLadder.Validator do
   """
 
   alias WordLadder.Ladder
+  alias WordLadder.Dictionary
 
   def valid_move?(%Ladder{start_word: start_word, word_list: []}, guessed_word),
     do: valid_move?(start_word, guessed_word)
@@ -12,6 +13,10 @@ defmodule WordLadder.Validator do
     do: valid_move?(guessed_word, last_guessed_word)
 
   def valid_move?(word1, word2) do
+    word2 in Dictionary.words() and one_change?(word1, word2)
+  end
+
+  defp one_change?(word1, word2) do
     String.graphemes(word1)
     |> Enum.zip(String.graphemes(word2))
     |> Enum.reduce(0, fn {a, b}, acc -> if a == b, do: acc, else: acc + 1 end)
